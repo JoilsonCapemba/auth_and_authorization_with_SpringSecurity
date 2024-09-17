28a393b260ed957d7c829a53d1ef8fc45a86a3dc
Joilson Capemba <joilsoncapemba@gmail.com>
2024-09-17 16:49:35 +0100

    first commit

---
+++ src/main/java/com/especialization/auth_and_authorization_with_SpringSecurity/AuthAndAuthorizationWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.especialization.auth_and_authorization_with_SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAndAuthorizationWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAndAuthorizationWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/especialization/auth_and_authorization_with_SpringSecurity/AuthAndAuthorizationWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.especialization.auth_and_authorization_with_SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAndAuthorizationWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
